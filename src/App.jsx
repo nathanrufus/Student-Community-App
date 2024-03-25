141ac32dfe3ea6eb1aba48467b02a46a0edc7c0d
kibet nathan <nathanrufus540@gmail.com>
2024-03-25 20:53:52 +0300

    Added signup component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Signin from "./components/Signin";
+import Signup from "./components/Signup";
 
 function App() {
   return <div>
     <Navbar/>
-    <Signin/>
+    {/* <Signin/> */}
+    <Signup/>
   </div>;
 }
 
